5f561c113ed586aab29b5ecf1332587aab6ff52b
Navid Khezrian <132568028+NavidKhezrian@users.noreply.github.com>
2023-10-16 13:04:45 +0200

    Update jobfile.sh

--- jobfile.sh
+++ jobfile.sh
@@ -8,4 +8,4 @@
 
 unset SLURM_EXPORT_ENV
 
-singularity run --bind /home/vault/b116ba/b116ba17/workshop/output:/user/source/output --bind /home/vault/b116ba/b116ba17/workshop/stable-diffusion-v1-5:/user/source/stable-diffusion-v1-5 workshop.sif --prompt "Iron Man, (Arnold Tsang, Toru Nakayama), Masterpiece, Studio Quality, 6k , toa, toaair, 1boy, glowing, axe, mecha, science_fiction, solo, weapon, jungle , green_background, nature, outdoors, solo, tree, weapon, mask, dynamic lighting, detailed shading, digital texture painting"
+singularity run --bind /home/vault/b116ba/b116ba17/workshop/output:/user/source/output --bind /home/vault/b116ba/b116ba17/workshop/stable-diffusion-v1-5:/user/source/stable-diffusion-v1-5 workshop-d2s-image.sif --prompt "Iron Man, (Arnold Tsang, Toru Nakayama), Masterpiece, Studio Quality, 6k , toa, toaair, 1boy, glowing, axe, mecha, science_fiction, solo, weapon, jungle , green_background, nature, outdoors, solo, tree, weapon, mask, dynamic lighting, detailed shading, digital texture painting"
